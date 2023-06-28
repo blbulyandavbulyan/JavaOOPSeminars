2bb8c32c851dfe4574dc95d5616d96a8dfdd55d0
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-06-28 15:14:58 +0400

    removed unnecessary method from Display

--- src/main/java/services/Display.java
+++ src/main/java/services/Display.java
@@ -6,7 +6,6 @@ import java.util.Collection;
 
 public interface Display {
     void displayMessage(String msg);
-    void showAvailableProductTypes(Collection<Class<? extends Product>> productTypes);
     default void displayError(String errorMsg){
         displayMessage(errorMsg);
     }
