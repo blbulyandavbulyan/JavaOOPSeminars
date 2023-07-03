765c3598cdf6b858cf6ad954f5230d2e6cf0e90f
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-03 10:47:34 +0400

    Javadoc added into IMarketBehavior

--- src/main/java/org/blbulyandavbulyan/seminar2/supermarket/services/interfaces/IMarketBehavior.java
+++ src/main/java/org/blbulyandavbulyan/seminar2/supermarket/services/interfaces/IMarketBehavior.java
@@ -4,8 +4,24 @@ import org.blbulyandavbulyan.seminar2.supermarket.domen.Actor;
 
 import java.util.List;
 
+/**
+ * Данный интерфейс предоставляет абстракцию над поведением магазина
+ */
 public interface IMarketBehavior {
+    /**
+     * Принимает клиента в магазин
+     * @param actor клиент, которого нужно принять
+     */
     void acceptToMarket(Actor actor);
+
+    /**
+     * Выводит клиентов из магазина
+     * @param actors список клиентов, которых нужно вывести из магазина
+     */
     void releaseFromMarket(List<Actor> actors);
+
+    /**
+     * Управляющий и основной метод, отвечает за всю логику магазина
+     */
     void update();
 }
