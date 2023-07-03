e3a4eb86f6bd142b1890819a8a25af03acee45f0
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-03 10:32:44 +0400

    Javadoc added into OrdinaryClient

--- src/main/java/org/blbulyandavbulyan/seminar2/supermarket/domen/OrdinaryClient.java
+++ src/main/java/org/blbulyandavbulyan/seminar2/supermarket/domen/OrdinaryClient.java
@@ -1,6 +1,13 @@
 package org.blbulyandavbulyan.seminar2.supermarket.domen;
 
+/**
+ * Предоставляет самого обычного клиента с именем
+ */
 public class OrdinaryClient extends Actor{
+    /**
+     * Создаёт клиента с заданным именем
+     * @param name имя, с которым нужно создать клиента
+     */
     public OrdinaryClient(String name) {
         super(name);
     }
