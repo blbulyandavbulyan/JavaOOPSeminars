bbaef77777e55428c61e8663d721cbabcee01a1e
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-03 11:55:50 +0400

    In main added saving log into log file

--- src/main/java/org/blbulyandavbulyan/seminar2/supermarket/Main.java
+++ src/main/java/org/blbulyandavbulyan/seminar2/supermarket/Main.java
@@ -4,9 +4,14 @@ import org.blbulyandavbulyan.seminar2.supermarket.domen.OrdinaryClient;
 import org.blbulyandavbulyan.seminar2.supermarket.domen.SpecialClient;
 import org.blbulyandavbulyan.seminar2.supermarket.services.Market;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Market market = new Market();
+        FileHandler fileHandler = new FileHandler("log.txt");
+        market.getLogger().addHandler(fileHandler);
         market.acceptToMarket(new OrdinaryClient("Вася"));
         market.acceptToMarket(new SpecialClient("Петя", 1));
         market.acceptToMarket(new OrdinaryClient("Женя"));
