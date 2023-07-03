c8636a121073f56ebd37c987ebba105e28767feb
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-03 10:37:38 +0400

    Javadoc improved for SpecialClient

--- src/main/java/org/blbulyandavbulyan/seminar2/supermarket/domen/SpecialClient.java
+++ src/main/java/org/blbulyandavbulyan/seminar2/supermarket/domen/SpecialClient.java
@@ -1,7 +1,19 @@
 package org.blbulyandavbulyan.seminar2.supermarket.domen;
 
+/**
+ * Предоставляет VIP клиента
+ */
 public class SpecialClient extends Actor{
+    /**
+     * VIP ID клиента
+     */
     private int idVip;
+
+    /**
+     * Создаёт экземпляр VIP клиента
+     * @param name имя
+     * @param idVip VIP ID
+     */
     public SpecialClient(String name, int idVip) {
         super(name);
         this.idVip = idVip;
@@ -12,6 +24,10 @@ public class SpecialClient extends Actor{
         return name;
     }
 
+    /**
+     * Получает VIP ID клиента
+     * @return VIP ID
+     */
     public int getIdVip() {
         return idVip;
     }
