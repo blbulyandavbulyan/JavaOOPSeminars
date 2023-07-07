27bcd740a681ee0b086c47f28790b6456df1d390
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-07 16:54:24 +0400

    javadoc added to Student

--- src/main/java/org/blbulyandavbulyan/seminar3/studentapp/domen/Student.java
+++ src/main/java/org/blbulyandavbulyan/seminar3/studentapp/domen/Student.java
@@ -1,12 +1,29 @@
 package org.blbulyandavbulyan.seminar3.studentapp.domen;
 
+/**
+ * Предоставляет студента, наследника человека
+ */
 public class Student extends Person{
+    /**
+     * ИД студента
+     */
     private int id;
+
+    /**
+     * Создаёт студента с переданным возрастом, именем и ИД
+     * @param age возраст студента
+     * @param name имя студента
+     * @param id ИД студента
+     */
     public Student(int age, String name, int id) {
         super(age, name);
         this.id = id;
     }
 
+    /**
+     * Получает ИД студента
+     * @return ИД студента
+     */
     public int getId() {
         return id;
     }
