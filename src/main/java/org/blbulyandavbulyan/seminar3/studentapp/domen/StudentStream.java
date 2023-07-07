819a48ea7a4a1966c970de3e031fd327f31afb48
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-07 14:12:19 +0400

    Added implementation Iterable for StudentStream, also added some simple realization

--- src/main/java/org/blbulyandavbulyan/seminar3/studentapp/domen/StudentStream.java
+++ src/main/java/org/blbulyandavbulyan/seminar3/studentapp/domen/StudentStream.java
@@ -1,8 +1,27 @@
 package org.blbulyandavbulyan.seminar3.studentapp.domen;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Поток студентов<br>
  * <b>Этот класс не имеет ничего общего с {@link java.util.stream.Stream}!</b>
  */
-public class StudentStream {
+public class StudentStream implements Iterable<StudentGroup>{
+    private List<StudentGroup> groups;
+    public StudentStream(List<StudentGroup> groups) {
+        this.groups = groups;
+    }
+    public StudentStream(){
+        this(new ArrayList<>());
+    }
+    @Override
+    public Iterator<StudentGroup> iterator() {
+        //поскольку в списве уже и так есть итератор, и его просто верну отсюда
+        return groups.iterator();
+    }
+    public void add(StudentGroup studentGroup){
+        this.add(studentGroup);
+    }
 }
