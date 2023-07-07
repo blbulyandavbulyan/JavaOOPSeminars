9d4b32d71199e29d42dbb1517f502cd8eb1fa697
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-07 14:00:58 +0400

    Person class added to studentapp

---
+++ src/main/java/org/blbulyandavbulyan/seminar3/studentapp/domen/Person.java
@@ -0,0 +1,27 @@
+package org.blbulyandavbulyan.seminar3.studentapp.domen;
+
+public class Person {
+    protected int age;
+    protected String name;
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
