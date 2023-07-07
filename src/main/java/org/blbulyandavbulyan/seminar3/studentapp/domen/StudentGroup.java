5df372f8bcc452f579fe9ce6e04dc0a2794e5daa
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-07 16:43:28 +0400

    Javadoc added to StudentGroup

--- src/main/java/org/blbulyandavbulyan/seminar3/studentapp/domen/StudentGroup.java
+++ src/main/java/org/blbulyandavbulyan/seminar3/studentapp/domen/StudentGroup.java
@@ -1,31 +1,67 @@
 package org.blbulyandavbulyan.seminar3.studentapp.domen;
 
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Предоставляет группу студентов
+ */
 public class StudentGroup implements Comparable<StudentGroup>{
+    /**
+     * Список студентов, состоящих в данной группе
+     */
     private List<Student> students;
+    /**
+     * Идентификатор группы
+     */
     private int groupId;
 
+    /**
+     * Создаёт экземпляр группы с заданными студентами и идентификатором
+     * @param students студенты, которые будут включены в данную группу
+     * @param groupId идентификатор данной группы
+     */
     public StudentGroup(List<Student> students, int groupId) {
-        this.students = students;
+        this.students = new ArrayList<>(students);
         this.groupId = groupId;
     }
 
-    public List<Student> getGroup() {
-        return students;
+    /**
+     * Создаёт пустую группу с заданным идентификатором
+     * @param groupId идентификатор данной группы
+     */
+    public StudentGroup(int groupId){
+        this.groupId = groupId;
+        this.students = new ArrayList<>();
     }
 
-    public void setGroup(List<Student> group) {
-        this.students = group;
+    /**
+     * Добавляет студента в группе
+     * @param student студент, которого нужно добавить
+     */
+    public void add(Student student){
+        students.add(student);
     }
 
+    /**
+     * @return идентификатор данной группы
+     */
     public int getGroupId() {
         return groupId;
     }
 
+    /**
+     * Устанавливает новый идентификатор группы
+     * @param groupId новый идентификатор данной группы
+     */
     public void setGroupId(int groupId) {
         this.groupId = groupId;
     }
+
+    /**
+     * Получает размер данной группы
+     * @return количество студентов в данной группе
+     */
     public int size(){
         return students.size();
     }
