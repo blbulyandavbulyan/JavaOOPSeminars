14689e147538ecd88af0b7420d86a96ea6edc70d
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-07 14:01:15 +0400

    StudentGroup class added to studentapp

---
+++ src/main/java/org/blbulyandavbulyan/seminar3/studentapp/domen/StudentGroup.java
@@ -0,0 +1,29 @@
+package org.blbulyandavbulyan.seminar3.studentapp.domen;
+
+import java.util.List;
+
+public class StudentGroup {
+    private List<Student> group;
+    private int groupId;
+
+    public StudentGroup(List<Student> group, int groupId) {
+        this.group = group;
+        this.groupId = groupId;
+    }
+
+    public List<Student> getGroup() {
+        return group;
+    }
+
+    public void setGroup(List<Student> group) {
+        this.group = group;
+    }
+
+    public int getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(int groupId) {
+        this.groupId = groupId;
+    }
+}
