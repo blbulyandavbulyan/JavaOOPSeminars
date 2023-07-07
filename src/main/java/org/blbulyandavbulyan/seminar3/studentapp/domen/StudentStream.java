fa4a85ba0c3216f5d8df2725cb5a922844650645
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-07 14:08:09 +0400

    Empty class StudentStream was added

---
+++ src/main/java/org/blbulyandavbulyan/seminar3/studentapp/domen/StudentStream.java
@@ -0,0 +1,8 @@
+package org.blbulyandavbulyan.seminar3.studentapp.domen;
+
+/**
+ * Поток студентов<br>
+ * <b>Этот класс не имеет ничего общего с {@link java.util.stream.Stream}!</b>
+ */
+public class StudentStream {
+}
