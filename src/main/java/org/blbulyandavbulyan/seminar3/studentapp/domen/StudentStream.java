f269eb7da5d9ae81d3dfd12de470fbce89eabb03
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-07 16:36:00 +0400

    added javadoc to StudentStream, also streamNumber and groups are final

--- src/main/java/org/blbulyandavbulyan/seminar3/studentapp/domen/StudentStream.java
+++ src/main/java/org/blbulyandavbulyan/seminar3/studentapp/domen/StudentStream.java
@@ -8,33 +8,62 @@ import java.util.stream.Collectors;
  * <b>Этот класс не имеет ничего общего с {@link java.util.stream.Stream}!</b>
  */
 public class StudentStream implements Iterable<StudentGroup>{
-    private List<StudentGroup> groups;
-    private int number;
-    public StudentStream(List<StudentGroup> groups, int number) {
+    /**
+     * Группы, которые состоят в данном потоке
+     */
+    private final List<StudentGroup> groups;
+    /**
+     * Номер потока
+     */
+    private final int streamNumber;
+
+    /**
+     * Создаёт экземпляр потока с заданным номером и группами
+     * @param groups группы, которые будут включены в данный поток
+     * @param streamNumber номер потока
+     */
+    public StudentStream(List<StudentGroup> groups, int streamNumber) {
         this.groups = groups;
-        this.number = number;
+        this.streamNumber = streamNumber;
     }
-    public StudentStream(int number){
-        this(new ArrayList<>(), number);
+
+    /**
+     * Создаёт пустой поток с заданным номером
+     * @param streamNumber номер потока
+     */
+    public StudentStream(int streamNumber){
+        this(new ArrayList<>(), streamNumber);
     }
     @Override
     public Iterator<StudentGroup> iterator() {
         //поскольку в списве уже и так есть итератор, и его просто верну отсюда
         return groups.iterator();
     }
+
+    /**
+     * Добавляет группу в поток
+     * @param studentGroup группа, которую нужно добавить
+     */
     public void add(StudentGroup studentGroup){
         groups.add(studentGroup);
     }
+
+    /**
+     * Сортирует группы в потоке используя стандартный компаратор в группе
+     */
     public void sort(){
         Collections.sort(groups);
     }
-    public void sort(Comparator<StudentGroup> studentGroupComparator){
-        groups.sort(studentGroupComparator);
-    }
 
+    /**
+     * Сортирует группы в потоке сначала по размеру группы, а потом по идентификатору
+     */
+    public void sortByCountAndThanByGroupIdentifier(){
+        groups.sort(Comparator.comparing(StudentGroup::size).thenComparing(StudentGroup::getGroupId));
+    }
     @Override
     public String toString() {
-        return "StudentStream{" + "number=" + number +
+        return "StudentStream{" + "number=" + streamNumber +
                 ", groups=" + groups.stream().map(StudentGroup::toString).collect(Collectors.joining("\n\t", "[\n\t", "\n]")) +
                 '}';
     }
