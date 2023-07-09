da892835e1ed49340e76bdc0cc99801a74bb3f75
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-09 19:13:45 +0400

    Javadoc improved for new StudentGroup

--- src/main/java/org/blbulyandavbulyan/seminar3/studentapp/domen/StudentGroup.java
+++ src/main/java/org/blbulyandavbulyan/seminar3/studentapp/domen/StudentGroup.java
@@ -4,11 +4,23 @@ import org.blbulyandavbulyan.seminar3.studentapp.domen.protypes.StudentGroupProt
 
 import java.util.List;
 
+/**
+ * Предоставляет реализацию StudentGroupPrototype в которой будет Student
+ */
 public class StudentGroup extends StudentGroupPrototype<Student> {
+    /**
+     * Создаёт группу студентов
+     * @param students список студентов(типа Student)
+     * @param groupId ИД группы
+     */
     public StudentGroup(List<Student> students, int groupId) {
         super(students, groupId);
     }
 
+    /**
+     * Создаёт пустую группу с заданным ИД
+     * @param groupId ИД группы
+     */
     public StudentGroup(int groupId) {
         super(groupId);
     }
