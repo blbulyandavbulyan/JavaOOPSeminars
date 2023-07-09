d146c92f54e1b19d289bf0bbe48b91e038b15019
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-09 14:18:41 +0400

    Now Person extends GenericPerson, and Student extends GenericStudent

--- src/main/java/org/blbulyandavbulyan/seminar3/studentapp/domen/Student.java
+++ src/main/java/org/blbulyandavbulyan/seminar3/studentapp/domen/Student.java
@@ -1,39 +1,19 @@
 package org.blbulyandavbulyan.seminar3.studentapp.domen;
 
+import org.blbulyandavbulyan.seminar3.studentapp.domen.protypes.GenericStudent;
+
 /**
  * Предоставляет студента, наследника человека
  */
-public class Student extends Person{
-    /**
-     * ИД студента
-     */
-    private int id;
+public class Student extends GenericStudent<String, Integer, Integer> {
 
     /**
-     * Создаёт студента с переданным возрастом, именем и ИД
-     * @param age возраст студента
-     * @param name имя студента
-     * @param id ИД студента
+     * Создаёт экземпляр человека с переданным возрастом и именем
+     *
+     * @param age  возраст человека
+     * @param name имя человека
      */
-    public Student(int age, String name, int id) {
-        super(age, name);
-        this.id = id;
-    }
-
-    /**
-     * Получает ИД студента
-     * @return ИД студента
-     */
-    public int getId() {
-        return id;
-    }
-
-    @Override
-    public String toString() {
-        return "Student{" +
-                "id=" + id +
-                ", age=" + age +
-                ", name='" + name + '\'' +
-                '}';
+    public Student(Integer age, String name, Integer id) {
+        super(age, name, id);
     }
 }
