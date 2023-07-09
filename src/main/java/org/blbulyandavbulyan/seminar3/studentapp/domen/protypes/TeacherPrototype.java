e5efa5b86556aacb396d16a37ca012125ee44433
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-09 20:37:17 +0400

    TeacherPrototype was added

---
+++ src/main/java/org/blbulyandavbulyan/seminar3/studentapp/domen/protypes/TeacherPrototype.java
@@ -0,0 +1,28 @@
+package org.blbulyandavbulyan.seminar3.studentapp.domen.protypes;
+
+/**
+ * Предоставляет параметризованного учителя
+ * @param <NT> тип имени учителя
+ * @param <AT> тип возраста учителя(должен быть подтипом Number)
+ * @param <DT> тип академической степени учителя
+ */
+public class TeacherPrototype<NT, AT extends Number, DT> extends PersonPrototype<NT, AT>{
+    /**
+     * Академическая степень учителя
+     */
+    private DT acadDegree;
+    /**
+     * Создаёт экземпляр учителя с переданным возрастом и именем
+     *
+     * @param name имя учителя
+     * @param age  возраст учителя
+     * @param acadDegree академическая степень учителя
+     */
+    public TeacherPrototype(NT name, AT age, DT acadDegree) {
+        super(name, age);
+    }
+
+    public DT getAcadDegree() {
+        return acadDegree;
+    }
+}
