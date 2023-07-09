eb53a7c08db56fa886bdda4fe3c9f44d1d363eee
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-09 18:28:07 +0400

    Added very useful method "of" to PersonComparator

--- src/main/java/org/blbulyandavbulyan/seminar3/studentapp/utils/PersonComparator.java
+++ src/main/java/org/blbulyandavbulyan/seminar3/studentapp/utils/PersonComparator.java
@@ -5,7 +5,8 @@ import org.blbulyandavbulyan.seminar3.studentapp.domen.protypes.PersonPrototype;
 import java.util.Comparator;
 
 /**
- * Данный компаратор сравнивает generics Person с таким же, сначала сравнивает по имени, а затем по возрасту
+ * Данный компаратор сравнивает generics Person с таким же, сначала сравнивает по имени, а затем по возрасту<br>
+ * Для более удобного создания используете метод {@link PersonComparator#of(Class)}
  * @param <NT> тип имени в PersonPrototype
  * @param <AT> тип возраста в PersonPrototype(должен быть Number и Comparable)
  * @param <T> конкретный тип, который будет сравнивать данный компаратор, должен быть наследником PersonPrototype
@@ -26,5 +27,18 @@ public class PersonComparator<NT extends Comparable<NT>, AT extends Number & Com
     @Override
     public int compare(T o1, T o2) {
         return comparator.compare(o1, o2);
+    }
+
+    /**
+     * Создаёт PersonComparator из переданного класса, наследника PersonPrototype
+     * @param personPrototypeClass класс из которого нужно создать PersonComparator
+     * @return созданный на базе класса personPrototypeClass компаратор
+     * @param <AT> тип возраста в {@link PersonPrototype}
+     * @param <NT> тип имени в {@link PersonPrototype}
+     * @param <R> сам тип класса {@link PersonPrototype}
+     */
+    public static <AT extends Number & Comparable<AT>, NT extends Comparable<NT>, R extends PersonPrototype<NT, AT>> PersonComparator<NT, AT, R> of(Class<R> personPrototypeClass){
+        //несмотря на то, что параметр personPrototype здесь не используется, но он позволяет мне не указывать кучу остальных generic параметров, т.к. они выводятся автоматически компилятором
+        return new PersonComparator<>();
     }
 }
