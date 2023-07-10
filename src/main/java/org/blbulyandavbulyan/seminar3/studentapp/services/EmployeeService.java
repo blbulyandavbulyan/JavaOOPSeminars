18e93b2951d59c9d3aeea5cd86e84645814c3290
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-10 20:01:16 +0400

    Javadoc to EmployeeService added

--- src/main/java/org/blbulyandavbulyan/seminar3/studentapp/services/EmployeeService.java
+++ src/main/java/org/blbulyandavbulyan/seminar3/studentapp/services/EmployeeService.java
@@ -6,15 +6,33 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Предоставляет сервис сотрудников
+ */
 public class EmployeeService implements IPersonService<Employee>{
     private final List<Employee> employees;
+
+    /**
+     * Создаёт экземпляр сервиса сотрудников
+     */
     public EmployeeService() {
         this.employees = new ArrayList<>();
     }
+
+    /**
+     * Получает всех сотрудников
+     * @return неизменяемый список сотрудников
+     */
     @Override
     public List<Employee> getAll() {
         return Collections.unmodifiableList(employees);
     }
+
+    /**
+     * Создаёт сотрудника с заданным именем и возрастом
+     * @param firstName имя
+     * @param age возраст
+     */
     @Override
     public void create(String firstName, int age) {
         employees.add(new Employee(firstName, age, "unknown"));
