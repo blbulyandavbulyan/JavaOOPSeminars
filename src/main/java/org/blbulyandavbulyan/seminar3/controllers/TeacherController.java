e9b3367bbd77ba72e9edb63eb428cb437072833c
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-10 19:18:04 +0400

    TeacherController was added

---
+++ src/main/java/org/blbulyandavbulyan/seminar3/controllers/TeacherController.java
@@ -0,0 +1,18 @@
+package org.blbulyandavbulyan.seminar3.controllers;
+
+import org.blbulyandavbulyan.seminar3.studentapp.domen.Teacher;
+import org.blbulyandavbulyan.seminar3.studentapp.services.TeacherService;
+import org.blbulyandavbulyan.seminar3.studentapp.utils.PersonComparator;
+
+public class TeacherController implements IPersonController<Teacher> {
+    private final TeacherService teacherService;
+    public TeacherController() {
+        teacherService = new TeacherService();
+    }
+
+    @Override
+    public void create(String firstName, int age) {
+        teacherService.create(firstName, age);
+        teacherService.sort(new PersonComparator<>());
+    }
+}
