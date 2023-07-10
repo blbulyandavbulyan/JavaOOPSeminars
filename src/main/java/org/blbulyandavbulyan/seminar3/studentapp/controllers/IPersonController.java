d27658c63bd10936bc50219d2d61f1bd9f188446
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-10 19:47:51 +0400

    javadoc added to IPersonController

--- src/main/java/org/blbulyandavbulyan/seminar3/studentapp/controllers/IPersonController.java
+++ src/main/java/org/blbulyandavbulyan/seminar3/studentapp/controllers/IPersonController.java
@@ -2,6 +2,15 @@ package org.blbulyandavbulyan.seminar3.studentapp.controllers;
 
 import org.blbulyandavbulyan.seminar3.studentapp.domen.protypes.PersonPrototype;
 
+/**
+ * Предоставляет обобщённый контроллер над персонами
+ * @param <PT> тип персоны, которыми будет оперировать контроллер
+ */
 public interface IPersonController <PT extends PersonPrototype<String, Integer>> {
+    /**
+     * Метод создаёт персону с заданным именем и возрастом
+     * @param firstName имя будущей персоны
+     * @param age возраст будущей персоны
+     */
     void create(String firstName, int age);
 }
