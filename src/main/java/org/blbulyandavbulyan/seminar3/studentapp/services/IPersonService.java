502a3856dc98da6d3fee36afdbc5fe5c2a4370db
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-10 20:01:28 +0400

    Javadoc to IPersonService added

--- src/main/java/org/blbulyandavbulyan/seminar3/studentapp/services/IPersonService.java
+++ src/main/java/org/blbulyandavbulyan/seminar3/studentapp/services/IPersonService.java
@@ -4,7 +4,21 @@ import org.blbulyandavbulyan.seminar3.studentapp.domen.protypes.PersonPrototype;
 
 import java.util.List;
 
+/**
+ * Сервис для персон
+ * @param <PT> тип персоны, обрабатываемой данным сервисом
+ */
 public interface IPersonService <PT extends PersonPrototype<String, Integer>> {
+    /**
+     * Получает всех персон
+     * @return неизменяемый список персон
+     */
     List<PT> getAll();
+
+    /**
+     * Создаёт персону с заданным именем и возрастом
+     * @param name имя будущей персоны
+     * @param age возраст будущей персоны
+     */
     void create(String name, int age);
 }
