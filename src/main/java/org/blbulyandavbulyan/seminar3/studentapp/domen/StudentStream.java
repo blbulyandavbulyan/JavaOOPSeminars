f732f1026b175c018fdfd556bf477726bfb96e2d
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-10 19:55:43 +0400

    Javadoc to new StudentStream added

--- src/main/java/org/blbulyandavbulyan/seminar3/studentapp/domen/StudentStream.java
+++ src/main/java/org/blbulyandavbulyan/seminar3/studentapp/domen/StudentStream.java
@@ -4,11 +4,23 @@ import org.blbulyandavbulyan.seminar3.studentapp.domen.protypes.StudentStreamPro
 
 import java.util.List;
 
+/**
+ * Предоставляет поток студентов, в котором в качестве группы хранится StudentGroup
+ */
 public class StudentStream extends StudentStreamPrototype<StudentGroup> {
+    /**
+     * Создаёт поток студентов с заданными группами и номером
+     * @param groups группы, с которыми будет создан поток
+     * @param streamNumber номер потока
+     */
     public StudentStream(List<StudentGroup> groups, int streamNumber) {
         super(groups, streamNumber);
     }
 
+    /**
+     * Создаёт поток студентов не содержащий групп
+     * @param streamNumber номер потока
+     */
     public StudentStream(int streamNumber) {
         super(streamNumber);
     }
