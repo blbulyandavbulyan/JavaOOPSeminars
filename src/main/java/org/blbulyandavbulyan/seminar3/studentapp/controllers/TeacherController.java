41160ad54348151841a279f6af8c129eee55ff69
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-10 19:49:46 +0400

    javadoc added to TeacherController

--- src/main/java/org/blbulyandavbulyan/seminar3/studentapp/controllers/TeacherController.java
+++ src/main/java/org/blbulyandavbulyan/seminar3/studentapp/controllers/TeacherController.java
@@ -4,12 +4,27 @@ import org.blbulyandavbulyan.seminar3.studentapp.domen.Teacher;
 import org.blbulyandavbulyan.seminar3.studentapp.services.TeacherService;
 import org.blbulyandavbulyan.seminar3.studentapp.utils.PersonComparator;
 
+/**
+ * Предоставляет контроллер над учителями
+ */
 public class TeacherController implements IPersonController<Teacher> {
+    /**
+     * Сервис с учителями
+     */
     private final TeacherService teacherService;
+
+    /**
+     * Создаёт экземпляр контроллера
+     */
     public TeacherController() {
         teacherService = new TeacherService();
     }
 
+    /**
+     * Создаёт учителя и сортирует всех учителей в сервисе
+     * @param firstName имя будущей персоны
+     * @param age возраст будущей персоны
+     */
     @Override
     public void create(String firstName, int age) {
         teacherService.create(firstName, age);
