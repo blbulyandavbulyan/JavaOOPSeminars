a696f721f9e1d05c80beb5f2e4bb908be8580e14
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-13 17:38:30 +0400

    Main added to studentmvc

---
+++ src/main/java/org/blbulyandavbulyan/seminar5/studentmvc/Main.java
@@ -0,0 +1,16 @@
+package org.blbulyandavbulyan.seminar5.studentmvc;
+
+import org.blbulyandavbulyan.seminar5.studentmvc.controllers.Controller;
+import org.blbulyandavbulyan.seminar5.studentmvc.models.HashModel;
+import org.blbulyandavbulyan.seminar5.studentmvc.views.ConsoleView;
+
+import java.util.ResourceBundle;
+
+public class Main {
+    public static void main(String[] args) {
+        ConsoleView consoleView = new ConsoleView(System.out, System.in, ResourceBundle.getBundle("locales/studentmvc/console-text"));
+        HashModel hashModel = new HashModel();
+        Controller controller = new Controller(hashModel, consoleView);
+        controller.run();
+    }
+}
