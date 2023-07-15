1d306fcb5fb21a0e6630252e2fca2e8e139a367d
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-15 14:01:12 +0400

    Added StudentAlreadyExistsException

---
+++ src/main/java/org/blbulyandavbulyan/seminar5/studentmvc/controllers/exceptions/StudentAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package org.blbulyandavbulyan.seminar5.studentmvc.controllers.exceptions;
+
+/**
+ * Исключение, которое бросается в случае если студент уже существует в модели
+ */
+public class StudentAlreadyExistsException extends RuntimeException{
+}
