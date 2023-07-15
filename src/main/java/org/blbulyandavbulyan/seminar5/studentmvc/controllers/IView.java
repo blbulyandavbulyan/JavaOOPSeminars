f4c7139ceb98dd6536c675b61f61b4e16f305aea
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-15 14:38:38 +0400

    javadoc for IView added

--- src/main/java/org/blbulyandavbulyan/seminar5/studentmvc/controllers/IView.java
+++ src/main/java/org/blbulyandavbulyan/seminar5/studentmvc/controllers/IView.java
@@ -4,11 +4,33 @@ import org.blbulyandavbulyan.seminar3.studentapp.domen.Student;
 
 import java.util.Collection;
 
+/**
+ * Данный интерфейс предоставляет view для контроллера
+ */
 public interface IView {
+    /**
+     * Метод отображает студентов
+     * @param students студенты, которых нужно отобразить
+     */
     void displayStudents(Collection<Student> students);
     void updateView(Collection<Student> students);
+
+    /**
+     * Метод получает ИД студента для удаления
+     * @return ИД студента, которого нужно удалить
+     */
     int getStudentIdForDelete();
     Student selectStudentForUpdate(Collection<Student> students);
+
+    /**
+     * Метод получает студента для добавления
+     * @return студент, которого нужно добавить
+     */
     Student getStudentForAdd();
+
+    /**
+     * Метод получает команду для обработки в цикле команд
+     * @return команда, которую нужно обработать
+     */
     Command getCommand();
 }
