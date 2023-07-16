067561effdd40a848f5ac46c151962f974e66de1
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-16 14:59:02 +0400

    Added rectangle and square task

---
+++ src/main/java/org/blbulyandavbulyan/seminar6/solid/rectangles/Rectangle.java
@@ -0,0 +1,18 @@
+package org.blbulyandavbulyan.seminar6.solid.rectangles;
+
+public class Rectangle {
+    private int width;
+    private int height;
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int area() {
+        return this.width * this.height;
+    }
+}
---
+++ src/main/java/org/blbulyandavbulyan/seminar6/solid/rectangles/Square.java
@@ -0,0 +1,15 @@
+package org.blbulyandavbulyan.seminar6.solid.rectangles;
+
+public class Square extends Rectangle {
+    @Override
+    public void setWidth(int width) {
+        super.setWidth(width);
+        super.setHeight(width);
+    }
+
+    @Override
+    public void setHeight(int height) {
+        super.setWidth(height);
+        super.setHeight(height);
+    }
+}
