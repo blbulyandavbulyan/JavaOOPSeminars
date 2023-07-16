b4a2d88e57e692736eeaaa23b5a5f7cb4fabd6e6
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-16 15:14:08 +0400

    Now Square is AbstractRectangle, also removed method for setting height and width, added method setSide

--- src/main/java/org/blbulyandavbulyan/seminar6/solid/rectangles/Square.java
+++ src/main/java/org/blbulyandavbulyan/seminar6/solid/rectangles/Square.java
@@ -1,15 +1,8 @@
 package org.blbulyandavbulyan.seminar6.solid.rectangles;
 
-public class Square extends Rectangle {
-    @Override
-    public void setWidth(int width) {
-        super.setWidth(width);
-        super.setHeight(width);
-    }
-
-    @Override
-    public void setHeight(int height) {
-        super.setWidth(height);
-        super.setHeight(height);
+public class Square extends AbstractRectangle {
+    public void setSide(int a){
+        super.setWidth(a);
+        super.setHeight(a);
     }
 }
