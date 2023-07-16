ce9e8b987de09e5df69fc1bdd5136c6c2693ec45
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-16 13:57:39 +0400

    Added Employee task

---
+++ src/main/java/org/blbulyandavbulyan/seminar6/solid/employees/Employee.java
@@ -0,0 +1,24 @@
+package org.blbulyandavbulyan.seminar6.solid.employees;
+
+import java.util.Date;
+
+public class Employee {
+    private String name;
+    private Date dob;
+    private int baseSalary;
+
+    public Employee(String name, Date dob, int baseSalary) {
+        this.name = name;
+        this.dob = dob;
+        this.baseSalary = baseSalary;
+    }
+
+    public String getEmpInfo() {
+        return "name - " + name + " , dob - " + dob.toString();
+    }
+
+    public int calculateNetSalary() {
+        int tax = (int) (baseSalary * 0.25);//calculate in otherway
+        return baseSalary - tax;
+    }
+}
