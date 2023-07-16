69820447f1ed2ac4c0c80141c5e516e547f75dd5
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-16 12:12:31 +0400

    Added language selection in Main

--- src/main/java/org/blbulyandavbulyan/seminar5/studentmvc/Main.java
+++ src/main/java/org/blbulyandavbulyan/seminar5/studentmvc/Main.java
@@ -2,19 +2,30 @@ package org.blbulyandavbulyan.seminar5.studentmvc;
 
 import org.blbulyandavbulyan.seminar5.studentmvc.controllers.Controller;
 import org.blbulyandavbulyan.seminar5.studentmvc.models.HashModel;
-import org.blbulyandavbulyan.seminar5.studentmvc.views.ConsoleView;
+import org.blbulyandavbulyan.seminar5.studentmvc.views.LocalizedConsoleView;
 
+import java.util.Arrays;
 import java.util.List;
-import java.util.ResourceBundle;
+import java.util.Scanner;
 
 /**
  * Точка входа в приложение
  */
 public class Main {
     public static void main(String[] args) {
-        ConsoleView consoleView = new ConsoleView(System.out, System.in, ResourceBundle.getBundle("locales/studentmvc/console-text"));
+        LocalizedConsoleView localizedConsoleView = createLocalizedConsoleView();
         HashModel hashModel = new HashModel();
-        Controller controller = new Controller(List.of(hashModel), consoleView);
+        Controller controller = new Controller(List.of(hashModel), localizedConsoleView);
         controller.run();
     }
+    private static AvailableLanguage selectLanguage(){
+        System.out.println("Available languages: " + Arrays.toString(AvailableLanguage.values()));
+        System.out.println("Choose language: ");
+        Scanner sc = new Scanner(System.in);
+        String languageName = sc.nextLine();
+        return AvailableLanguage.valueOf(languageName);
+    }
+   private static LocalizedConsoleView createLocalizedConsoleView(){
+        return new LocalizedConsoleView(System.out, System.in, selectLanguage().getShortName());
+   }
 }
