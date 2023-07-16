2013be2a8a6181976c6f13859da9545454dec48d
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-07-16 13:17:43 +0400

    Constructor signature changed, removed type from parameters

--- src/main/java/org/blbulyandavbulyan/seminar6/solid/vehicles/Bus.java
+++ src/main/java/org/blbulyandavbulyan/seminar6/solid/vehicles/Bus.java
@@ -1,8 +1,8 @@
 package org.blbulyandavbulyan.seminar6.solid.vehicles;
 
 public class Bus extends Vehicle{
-    public Bus(int maxSpeed, String type) {
-        super(maxSpeed, type);
+    public Bus(int maxSpeed) {
+        super(maxSpeed, "Bus");
     }
 
     @Override
